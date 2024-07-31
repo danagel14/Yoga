516e04c813d87bfee94ef31c78fb7831876d849c
danagel14 <danagel14@gmail.com>
2024-07-31 23:06:20 +0300

    css

--- E-commerce-main/src/routes/index.js
+++ E-commerce-main/src/routes/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const adminOnly = require("../middlewares/adminOnly");
 const auth = require("../middlewares/auth");
 const { showShop, showCart, showMenu} = require("../controllers/global");
+const { getOrdersStatusCount } = require('../controllers/orders');
 
 router.use("/user", require("./user"));
 router.use("/user", auth, require("./orders"));
@@ -11,7 +12,7 @@ router.get("/cart", auth, showCart);
 router.get("/home", auth, showMenu);
 router.get("/menu", auth, showShop);
 
-
+router.get('/api/orders/status', getOrdersStatusCount);
 router.use("/admin", adminOnly, require("./dashboard"));
 
 router.use("/admin", adminOnly, require("./accounts"));
