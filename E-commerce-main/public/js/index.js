091e56c6dc77bda7e5ec053cca707b286d6f0e2c
danagel14 <danagel14@gmail.com>
2024-08-01 21:56:22 +0300

    fix search

--- E-commerce-main/public/js/index.js
+++ E-commerce-main/public/js/index.js
@@ -63,67 +63,51 @@ tempElement.addEventListener("mouseout", function () {
     }
 
     function updateProductList(products, role) {
-    const shopSection = $(".shop");
-    shopSection.empty();
-    let productHtml = ``;
-
-    products.map(product => {
-      productHtml += `
-            <div class="product">
-                <span>${product.productName}</span>
-                <div class="image">
-                    <img src="${product.pathImage}" alt="product" />
-                </div>
-                <span class="data">
-                    <span>${product.price}&#8362;</span>
-                    <span>${product.description}</span>
-                </span>
-                <div class="actions">
-                    <button
-                        class="add"
-                        data-product='${JSON.stringify(product)}'
-                        onclick="Cart.handleCart(this)"
-                    >
-                        Add
-                    </button>`
-                  if(role === 'ADMIN'){
-                        productHtml +=  `<button
-                        value="${product._id}"
-                        onclick="showPopup(this.value)"
-                        class="delete"
-                    >
-                        Delete
-                    </button>
-                    <button class="edit">
-                        <a href="/admin/dashboard/update-product/${product._id}"> Edit </a>
-                    </button>
-                    </div>
-                    </div>`
-                  }
-    })
-
-    shopSection.append(productHtml);
-}
-
-      function handleDelete() {
-        if (!itemId) return;
-        $.ajax({
-          url: "/admin/delete-product",
-          method: "DELETE",
-          contentType: "application/json",
-          data: JSON.stringify({
-            id: itemId,
-          }),
-          success: function (response) {
-            document.getElementById("popup").style.display = "none";
-            location.reload();
-          },
-          error: function (error) {
-            $("#message_error").text(error?.responseJSON?.message);
-          },
-        });
-      }
-
+      const shopSection = $(".shop");
+      shopSection.empty();
+      let productHtml = ``;
+    
+      products.map((product) => {
+        productHtml += `
+          <div class="product">
+            <span>${product.productName}</span>
+            <div class="image">
+              <img src="${product.pathImage}" alt="product" />
+            </div>
+            <span class="data">
+              <span>${product.price}&#8362;</span>
+              <span>${product.description}</span>
+            </span>
+            <div class="actions">
+              <button
+                class="add"
+                data-product='${JSON.stringify(product)}'
+                onclick="Cart.handleCart(this)"
+              >
+                Add
+              </button>`;
+    
+        if (role === "ADMIN") {
+          productHtml += `
+              <button
+                value="${product._id}"
+                onclick="showPopup(this.value)"
+                class="delete"
+              >
+                Delete
+              </button>
+              <button class="edit">
+                <a href="/admin/dashboard/update-product/${product._id}"> Edit </a>
+              </button>`;
+        }
+    
+        productHtml += `
+            </div>
+          </div>`;
+      });
+    
+      shopSection.append(productHtml);
+    }
       function showPopup(productId) {
         itemId = productId;
         document.getElementById("popup").style.display = "flex";
